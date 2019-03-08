f24807dd4050faece1f52b821c9e9e0fa7809d65
Győző Horváth <gyozo.horvath@inf.elte.hu>
2019-03-08 12:25:53 +0100

    refactor dropdown component

--- src/components/dropdown-allow-addition.tsx
+++ src/components/dropdown-allow-addition.tsx
@@ -1,59 +1,50 @@
-import React, {Component, SyntheticEvent} from 'react'
-import {Dropdown, DropdownProps} from 'semantic-ui-react'
+import React, {SyntheticEvent, useEffect, useState, KeyboardEvent} from 'react'
+import {Dropdown, DropdownProps, DropdownItemProps} from 'semantic-ui-react'
 
+interface IProps {
+    maxElement : number, 
+    protocol : string
+};
 const pattern = /[a-z]+:[0-9]+/i;
-class DropdownAllowAdditions extends Component<{maxElement : Number, protocol : String},any> {
-    constructor(props : any) {
-        super(props);
-        var recent =  localStorage.getItem(this.props.protocol.concat('Recent'));
-        var recentArray = recent !== null ? recent.split(';') : [];
-        var options : any[] = [];
-        for(var i=0;i<recentArray.length;i++)
-        {
-            options[i]= {key: recentArray[i], text: recentArray[i], value: recentArray[i]}
-        }
-        this.state = { options };
-    }
 
-    // @ts-ignore
-    handleAddition = (e : KeyboardEvent<HTMLElement>, { value } : DropdownProps) => {
+export const DropdownAllowAdditions = ({maxElement, protocol}: IProps) => {
+    const [currentValue, setCurrentValue] = useState('')
+    const [options, setOptions] = useState<DropdownItemProps[]>([])
+
+    useEffect(() => {
+        const recent =  localStorage.getItem(protocol.concat('Recent')) || ""
+        const recentArray = recent.split(';')
+        setOptions(recentArray.map(e => ({key: e, text: e, value: e})))
+    }, [protocol])
+
+    const handleAddition = (e : KeyboardEvent<HTMLElement>, { value } : DropdownProps) => {
         if (value == null) return;
         if(pattern.test(value.toString())) {
-            if (this.props.maxElement <= this.state.options.length) {
-                this.state.options.splice(-1, 1);
+            if (maxElement <= options.length) {
+                options.splice(-1, 1);
             }
-            this.setState({
-                options: [{key: value, text: value, value: value}, ...this.state.options],
-            })
-            var save = "";
-            for(var i=0;i<this.state.options.length;++i) {
-                save = save + this.state.options[i].text+';';
-            }
-            save = save.substring(0, save.length - 1);
-            localStorage.setItem(this.props.protocol.concat('Recent'), save);
+            const newOptions = [{key: value as string, text: value as string, value: value as string}, ...options]
+            setOptions(newOptions)
+            const save = newOptions.map(e => e.text).join(';')
+            localStorage.setItem(protocol.concat('Recent'), save);
         }
     };
 
-    handleChange = (e : SyntheticEvent<HTMLElement, Event>, { value } : DropdownProps) => 
-    this.setState({ currentValue: value });
+    const handleChange = (e : SyntheticEvent<HTMLElement, Event>, { value } : DropdownProps) => {
+        setCurrentValue(value as string);
+    } 
 
-    render() {
-        const { currentValue } : any = this.state;
-
-        return (
-            <Dropdown
-                options={this.state.options}
-                placeholder='Select host'
-                search
-                selection
-                fluid
-                allowAdditions
-                value={currentValue}
-                onAddItem={this.handleAddition}
-                onChange={this.handleChange}
-            />
-        )
-    }
+    return (
+        <Dropdown
+            options={options}
+            placeholder='Select host'
+            search
+            selection
+            fluid
+            allowAdditions
+            value={currentValue}
+            onAddItem={handleAddition}
+            onChange={handleChange}
+        />
+    )
 }
-
-export default DropdownAllowAdditions
--- src/components/modals/settings-modal.component.tsx
+++ src/components/modals/settings-modal.component.tsx
@@ -1,43 +1,53 @@
 import React, { useState } from "react";
 import { Button, Modal, Icon } from "semantic-ui-react";
-import DropdownAllowAdditions from "../dropdown-allow-addition";
+import { DropdownAllowAdditions } from "../dropdown-allow-addition";
 
 export const SettingsModal = () => {
-    const [open, setOpen] = useState(false);
-    const [protocol, setProtocol] = useState('spark')
-    return (
-        <Modal
-            trigger={
-                <a className="item" onClick={() => setOpen(true)}>
-                    <i className="cog icon" /> Settings
-                </a>
-            }
-            closeIcon
-            open={open}
-            onClose={() => setOpen(false)}
-            closeOnDimmerClick={false}
-        >
-            <Modal.Header>Settings</Modal.Header>
-            <Modal.Content>
-            <Button.Group>
-                <Button positive={protocol==='spark'} onClick={() => setProtocol('spark')}>Spark</Button>
-                <Button.Or />
-                <Button positive={protocol==='flink'} onClick={() => setProtocol('flink')}>Flink</Button>
-            </Button.Group>
-                <div>
-                    <DropdownAllowAdditions maxElement={7} protocol={protocol}/>
-                </div>
-                <div className="ui form">
-                    <div className="field">
-                        <textarea />
-                    </div>
-                </div>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button color="green" onClick={() => setOpen(false)}>
-                    <Icon name="checkmark" /> Got it
-                </Button>
-            </Modal.Actions>
-        </Modal>
-    );
+  const [open, setOpen] = useState(false);
+  const [protocol, setProtocol] = useState("spark");
+  return (
+    <Modal
+      trigger={
+        <a className="item" onClick={() => setOpen(true)}>
+          <i className="cog icon" /> Settings
+        </a>
+      }
+      closeIcon
+      open={open}
+      onClose={() => setOpen(false)}
+      closeOnDimmerClick={false}
+    >
+      <Modal.Header>Settings</Modal.Header>
+      <Modal.Content>
+        <Button.Group>
+          <Button
+            positive={protocol === "spark"}
+            onClick={() => setProtocol("spark")}
+          >
+            Spark
+          </Button>
+          <Button.Or />
+          <Button
+            positive={protocol === "flink"}
+            onClick={() => setProtocol("flink")}
+          >
+            Flink
+          </Button>
+        </Button.Group>
+        <div>
+          <DropdownAllowAdditions maxElement={7} protocol={protocol} />
+        </div>
+        <div className="ui form">
+          <div className="field">
+            <textarea />
+          </div>
+        </div>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="green" onClick={() => setOpen(false)}>
+          <Icon name="checkmark" /> Got it
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
 };
